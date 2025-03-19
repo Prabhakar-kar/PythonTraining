ad7a0ff2e2d5dc707f9d64cc09b42fce3b1d6347
Prabhakar-kar <prabhakarcourse01@gmail.com>
2025-03-19 07:53:35 +0530

    Add files via upload

---
+++ assignment_9_2.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gender = input("Enter your gender: ")
+city = input("Enter your city: ")
+japanese_lvl = input("Enter your Japanese level: ")
+
+infoTuple = name, age, gender, city, japanese_lvl
+
+print(infoTuple)
