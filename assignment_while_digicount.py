2a65240adc2cf2fe00ffdbe797976aad8e554695
Prabhakar-kar <prabhakarcourse01@gmail.com>
2025-03-28 10:05:35 +0530

    Add files via upload

---
+++ assignment_while_digicount.py
@@ -0,0 +1,6 @@
+digit = str(input("Enter number to count digit: "))
+
+index = 0
+while index < len(digit):
+    index += 1
+print("Digit count: ", len(digit))
---
+++ assignment_while_sum.py
@@ -0,0 +1,8 @@
+sum_num = 0
+i = 1
+
+while i <= 50:
+    sum_num += i
+    i += 1
+
+print(f"Sum of numbers from 1 to 50 is {sum_num}")
